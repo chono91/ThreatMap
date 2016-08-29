1d38cc31b3a9bea847cd19ae04ffcb87e4aaa3d8
chono91 <chono91@gmail.com>
2016-08-28 21:33:56 -0400

    Sample graph set up, need to import data into it. Added seperate resource and raw data feilds to the return data from the server

--- back-end/classes/dataInterface.js
+++ back-end/classes/dataInterface.js
@@ -9,7 +9,8 @@ module.exports = {
         var info = threatFeed.data(req.body.md5);   //pass md5 to plug in
             info.then( (data) => { // wait for data to come in
                 threatFeeds[i].setData(JSON.stringify(data)); // set the raw data field in the class to the reply
-                var response = {"Resource" : threatFeeds[i]};
+                var response = {"Resource" : threatFeeds[i].name};
+                response.rawData = threatFeeds[i].rawData;
                 //console.log({Resource: threatFeeds[i].source });
                 //send the data, need to add graphing thing later
                 promise.resolve(response);
