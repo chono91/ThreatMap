aa29f0492a4901b28178e593a026ecff5b14ab4d
chono91 <chono91@gmail.com>
2016-08-28 22:26:04 -0400

    Moved back to sending key pairs

--- back-end/classes/dataInterface.js
+++ back-end/classes/dataInterface.js
@@ -9,8 +9,8 @@ module.exports = {
         var info = threatFeed.data(req.body.md5);   //pass md5 to plug in
             info.then( (data) => { // wait for data to come in
                 threatFeeds[i].setData(data); // set the raw data field in the class to the reply
-                var response = ({"Resource" : threatFeeds[i].name}, {"rawData" :  threatFeeds[i].rawData});
-                //response = JSON.stringify(response);
+                var response = ({"Resource" : threatFeeds[i].name})
+                response.rawData = threatFeeds[i].rawData;
                 console.log("RESP DATA: " + response.rawData[2]);
                 //console.log({Resource: threatFeeds[i].source });
                 //send the data, need to add graphing thing later
